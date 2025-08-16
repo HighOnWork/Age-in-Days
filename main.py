61dd9bcc841105146f69794e14e5eeca0b0a1a47
HighOnWork <highonworking@gmail.com>
2025-08-16 16:30:13 +0500

    Update main.py
    red added

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+Reset = '\033[0m'
+Red = '\033[31m'
+
 Age = int(input("Please enter your age: "))
 
 Final_Age = Age * 365.3
 
-print(f"You have lived for {Final_Age} number of days in total.")
+Final_Age = round(Final_Age, 0)
+print(f"You have lived for {Red} {Final_Age} {Reset} number of days in total.")
